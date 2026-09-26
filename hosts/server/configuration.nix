ebbdd102199ed49e703870e222a7f9248d3ec7b8
Dragan Nikolovski <dragannikolovski215@gmail.com>
2026-09-26 18:57:57 +0200

    a

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -2,13 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [
       ./hardware-configuration.nix
 	../../modules/common.nix
+	inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
